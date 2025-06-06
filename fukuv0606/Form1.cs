004ce90d9723feec3f8bb0f860213aef0b2c0667
Nikaido　Yuki <nikaido01101@gmail.com>
2025-06-06 13:28:52 +0900

    復習問題

--- fukuv0606/Form1.cs
+++ fukuv0606/Form1.cs
@@ -2,9 +2,31 @@ namespace fukuv0606
 {
     public partial class Form1 : Form
     {
+        int vx = -4;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            label1.Text = "nikaido yuki";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            if (label1.Left < 0)
+            {
+                vx = -vx;
+                label1.Left = 0;
+            }
+            else if (label1.Right > this.ClientSize.Width)
+            {
+                vx = -vx;
+                label1.Left = this.ClientSize.Width - label1.Width;
+            }
         }
     }
 }
